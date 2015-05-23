7bf98694e3157390281412db43cc177f83c16815
alyucia <althong@gmail.com>
2015-05-23 12:40:26 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2735,7 +2735,7 @@
 
             rouletteCommand: {
                 command: 'roulette',
-                rank: 'mod',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
