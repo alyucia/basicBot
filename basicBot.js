f40898cce6e411898859bbfb8795b2e9324b8457
alyucia <althong@gmail.com>
2015-05-23 17:09:17 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1599,7 +1599,7 @@
                 rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    //if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var author = API.getMedia().author;
