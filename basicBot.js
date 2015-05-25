f792a0cbb14c6fb7b1c3503cee5b72849faec2d4
alyucia <althong@gmail.com>
2015-05-24 17:53:02 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3381,7 +3381,7 @@
             	type: "startsWith",
             	functionality: function(chat, cmd){
         		if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-        	        if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+        	        //if (!basicBot.commands.executable(this.rank, chat)) return void (0);
         	 	else{
         	 		var perm = basicBot.userUtilities.getPermission(chat.uid);
                         	var msg = chat.message;
@@ -3417,7 +3417,7 @@
             		type: "startsWith",
             		functionality: function(chat, cmd){
         			if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-        		        if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+        		       // if (!basicBot.commands.executable(this.rank, chat)) return void (0);
         		 	else{
         		 		var perm = basicBot.userUtilities.getPermission(chat.uid);
                 	        	var msg = chat.message;
