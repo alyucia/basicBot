8c7f0e69a238e846b0de93030e49eebf70c1ce0c
alyucia <althong@gmail.com>
2015-05-24 17:47:08 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3179,7 +3179,7 @@
 
             usercommandsCommand: {
                 command: 'usercommands',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
